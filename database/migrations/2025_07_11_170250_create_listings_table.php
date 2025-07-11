45acef8011564358c77d38271ee2e0d63af91571
Milos Ilic <m.iwork71@gmail.com>
2025-07-11 19:48:05 +0200

    Added categories and listings migrations.

---
+++ database/migrations/2025_07_11_170250_create_listings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->enum('condition', ['new', 'used']);
+            $table->string('image_path')->nullable();
+            $table->string('phone', 50);
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
---
+++ database/migrations/2025_07_11_172852_add_add_foreign_keys_to_listings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            //
+            //Added foreign key constraints for the user_id, category_id columns.
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('listings', function (Blueprint $table) {
+            //
+            //Drop the foreign key constraints.
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['category_id']);
+        });
+    }
+};
